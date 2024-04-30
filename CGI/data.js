79d74371121ed58d9f791e872793513dcd701649
Blightop <153464874+Blightop@users.noreply.github.com>
2024-04-30 13:00:41 +0100

    Add files via upload

---
+++ CGI/data.js
@@ -0,0 +1,20 @@
+const chart = document.querySelector("#lineChart").getContext('2d')
+
+new Chart(chart, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
